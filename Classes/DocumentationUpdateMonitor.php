126543263bb35da82f70d819f53abea68c746090
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-18 10:16:34 +0000

    added a documentation update monitor that hooks into documenation rendering as a new "output format"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ter_doc_solrindexer/trunk@52116 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/DocumentationUpdateMonitor.php
@@ -0,0 +1,109 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * A class that hooks into the documentation rendering process disguised as an
+ * additional output format.
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @package	TYPO3
+ * @subpackage	ter_doc_solrindexer
+ */
+class Tx_TerDocSolrindexer_DocumentationUpdateMonitor extends tx_terdoc_documentformat_index {
+
+	/**
+	 * Cache directory for the rendered documentation.
+	 *
+	 * @var	string
+	 */
+	protected $documentDirectory;
+
+	/**
+	 * Documentation Renderer
+	 *
+	 * @var	tx_terdoc_renderdocuments
+	 */
+	protected $documentationRenderer;
+
+	/**
+	 * Indexes the documentation into Apache Solr using the files rendered by
+	 * EXT:ter_doc_html before.
+	 *
+	 * @param	string		$documentDir: Absolute directory for the document currently being processed.
+	 * @return	void
+	 */
+	public function renderCache($documentDir) {
+		$this->documentDirectory = $documentDir;
+		$this->documentationRenderer = tx_terdoc_renderdocuments::getInstance();
+
+		$extensionManualMetaData = $this->getExtensionManualMetaData();
+
+		$indexQueue = t3lib_div::makeInstance('tx_solr_indexqueue_Queue');
+		$indexQueue->updateItem('tx_terdoc_manuals', $extensionManualMetaData['uid']);
+	}
+
+	/**
+	 * Usually would return TRUE if a rendered document for the given extension
+	 * version is available. Since we only want to index but not display
+	 * documentation we always return FALSE.
+	 *
+	 * @param	string		$extensionKey: Extension key of the document
+	 * @param	string		$version: Version number of the document
+	 * @return	boolean		TRUE if rendered version is available, otherwise FALSE
+	 */
+	public function isAvailable($extensionKey, $version) {
+		return FALSE;
+	}
+
+	/**
+	 * Gets the full extension manual meta data record as created by the
+	 * documentation renderer.
+	 *
+	 * @return	array	Manual meta data record
+	 */
+	protected function getExtensionManualMetaData() {
+		$explodedPath = t3lib_div::trimExplode('/', $this->documentDirectory, TRUE);
+		$extensionKeyAndVersion = array_pop($explodedPath);
+		list($extensionKey, $extensionVersion) = explode('-', $extensionKeyAndVersion);
+
+		$extensionManualMetaData = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow(
+			'*',
+			'tx_terdoc_manuals',
+			'extensionkey = "' . $extensionKey . '"'
+				. ' AND version = "' . $extensionVersion . '"'
+				. ' AND pid = "' . intval($this->documentationRenderer->getStoragePid()) . '"'
+		);
+
+		return $extensionManualMetaData;
+	}
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/ter_doc_solrindexer/Classes/DocumentationUpdateMonitor.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/ter_doc_solrindexer/Classes/DocumentationUpdateMonitor.php']);
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,15 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+require_once(t3lib_extMgm::extPath('ter_doc_solrindexer') . 'classes/DocumentationUpdateMonitor.php');
+
+$renderDocsObj = tx_terdoc_renderdocuments::getInstance();
+$renderDocsObj->registerOutputFormat(
+	'ter_doc_solrindexer_IndexDocument',
+	'LLL:EXT:ter_doc_solrindexer/locallang.xml:format_solrIndexDocument',
+	'index',
+	t3lib_div::makeInstance('Tx_TerDocSolrindexer_DocumentationUpdateMonitor')
+);
 
 
 ?>
